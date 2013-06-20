fc70030ef61404f145bb9a8d2e7e120f4db83300
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-06-20 13:37:07 -0400

    Updated code for figures of Hardi reconstructions

--- code/hardi_reconstructions.py
+++ code/hardi_reconstructions.py
@@ -1,9 +1,11 @@
 import numpy as np
 import nibabel as nib
 from dipy.data import fetch_stanford_hardi, read_stanford_hardi, get_sphere
-from dipy.reconst.shm import CsaOdfModel, QballModel, sh_to_sf
+from dipy.reconst.shm import CsaOdfModel, QballModel
+from dipy.reconst.csdeconv import Csd
+from dipy.reconst.dti import TensorModel
 from dipy.viz import fvtk
-from dipy.reconst.odf import gfa
+
 
 fetch_stanford_hardi()
 img, gtab = read_stanford_hardi()
@@ -11,7 +13,16 @@ data = img.get_data()
 sphere = get_sphere('symmetric724')
 sphere2 = get_sphere('symmetric362')
 
-data_small  = data[20:50,55:85, 38:39]
+data_small  = data[20:50, 55:85, 38:39]
+
+dtimodel = TensorModel(gtab)
+dtifit = dtimodel.fit(data_small)
+dtiodfs = dtifit.odf(sphere)
+
+ren = fvtk.ren()
+fvtk.add(ren, fvtk.sphere_funcs(dtiodfs, sphere, colormap=None))
+fvtk.show(ren)
+fvtk.record(ren, n_frames=1, out_path='tensorodfs.png', size=(600, 600), magnification=4)
 
 order = 4
 qballmodel = QballModel(gtab, order, smooth=0.006)
@@ -23,7 +34,7 @@ ren = fvtk.ren()
 fvtk.add(ren, fvtk.sphere_funcs(qballodfs, sphere, colormap='jet'))
 fvtk.show(ren)
 #print('Saving illustration as qballodfs.png')
-#fvtk.record(ren, n_frames=1, out_path='qballodfs.png', size=(600, 600))
+fvtk.record(ren, n_frames=1, out_path='qballodfs.png', size=(600, 600), magnification=4)
 fvtk.clear(ren)
 
 # min-max normalize ODFs
@@ -33,7 +44,7 @@ minmax_odfs = (qballodfs - np.min(qballodfs, -1)[..., None]) / (np.max(qballodfs
 fvtk.add(ren, fvtk.sphere_funcs(minmax_odfs, sphere, colormap='jet'))
 fvtk.show(ren)
 #print('Saving illustration as qball_minmax_odfs.png')
-#fvtk.record(ren, n_frames=1, out_path='qball_minmax_odfs.png', size=(600, 600))
+fvtk.record(ren, n_frames=1, out_path='qball_minmax_odfs.png', size=(600, 600), magnification=4)
 fvtk.clear(ren)
 
 csamodel = CsaOdfModel(gtab, order, smooth=0.006)
@@ -46,7 +57,7 @@ ren = fvtk.ren()
 fvtk.add(ren, fvtk.sphere_funcs(csaodfs, sphere, colormap='jet'))
 fvtk.show(ren)
 #print('Saving illustration as csa_odfs.png')
-#fvtk.record(ren, n_frames=1, out_path='csa_odfs.png', size=(600, 600))
+fvtk.record(ren, n_frames=1, out_path='csa_odfs.png', size=(600, 600), magnification=4)
 fvtk.clear(ren)
 
 # notice the negative values produced
@@ -55,13 +66,8 @@ ren = fvtk.ren()
 fvtk.add(ren, fvtk.sphere_funcs(np.clip(csaodfs, 0, np.max(csaodfs, -1)[..., None]), sphere, colormap='jet'))
 fvtk.show(ren)
 #print('Saving illustration as csa_odfs_positive.png')
-#fvtk.record(ren, n_frames=1, out_path='csa_odfs_positive.png', size=(600, 600))
+fvtk.record(ren, n_frames=1, out_path='csa_odfs_positive.png', size=(600, 600), magnification=4)
 fvtk.clear(ren)
 
-from dipy.segment.mask import hist_mask
+# Constrained Spherical Deconvolution
 
-mask = hist_mask(data[...,0])
-
-csafit = csamodel.fit(data, mask)
-
-nib.save(nib.Nifti1Image(csafit.shm_coeff.astype('float32'), img.get_affine()), 'csa_odf_sh.nii.gz')
