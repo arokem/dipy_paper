497b42fc517dae40add128b02eae897fa0244319
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-07-18 17:32:47 -0400

    Adding ellipsoid visualization - still wip

--- code/hardi_reconstructions.py
+++ code/hardi_reconstructions.py
@@ -18,11 +18,41 @@ dtimodel = TensorModel(gtab)
 dtifit = dtimodel.fit(data_small)
 dtiodfs = dtifit.odf(sphere)
 
+
+def ellipsoids(evals, evecs, sphere):
+
+    from dipy.core.ndindex import ndindex
+
+    v = sphere.vertices
+    ellipsoid = np.zeros(evals.shape[:-1] + (v.shape[0],))
+    for index in ndindex(evals.shape[:-1]):
+        ea = np.diag(evals[index])
+        ev = evecs[index]
+
+        proj = np.dot(ea, v.T)
+
+        push = np.sqrt(proj[0, :]**2 + proj[1, :]**2 + proj[2, :]**2)
+        ellipsoid[index] = push
+
+    return ellipsoid
+
+
 ren = fvtk.ren()
 fvtk.add(ren, fvtk.sphere_funcs(dtiodfs, sphere, colormap=None))
+#fvtk.show(ren)
+fvtk.record(ren, n_frames=1, out_path='tensorodfs.png',
+            size=(600, 600), magnification=4)
+
+fvtk.clear(ren)
+
+els = ellipsoids(dtifit.evals, dtifit.evecs, sphere)
+
+fvtk.add(ren, fvtk.sphere_funcs(els, sphere, colormap=None, norm=False, radial_scale=True))
 fvtk.show(ren)
 fvtk.record(ren, n_frames=1, out_path='tensorodfs.png',
             size=(600, 600), magnification=4)
+
+1/0
 
 order = 4
 qballmodel = QballModel(gtab, order, smooth=0.006)
@@ -32,7 +62,7 @@ qballodfs = qballfit.odf(sphere)
 
 ren = fvtk.ren()
 fvtk.add(ren, fvtk.sphere_funcs(qballodfs, sphere, colormap='jet'))
-fvtk.show(ren)
+#fvtk.show(ren)
 # print('Saving illustration as qballodfs.png')
 fvtk.record(ren, n_frames=1, out_path='qballodfs.png',
             size=(600, 600), magnification=4)
@@ -44,7 +74,7 @@ minmax_odfs = (qballodfs - np.min(qballodfs, -1)[..., None]) / (
     np.max(qballodfs, -1)[..., None] - np.min(qballodfs, -1)[..., None])
 
 fvtk.add(ren, fvtk.sphere_funcs(minmax_odfs, sphere, colormap='jet'))
-fvtk.show(ren)
+#fvtk.show(ren)
 # print('Saving illustration as qball_minmax_odfs.png')
 fvtk.record(ren, n_frames=1, out_path='qball_minmax_odfs.png',
             size=(600, 600), magnification=4)
@@ -58,7 +88,7 @@ csaodfs = csafit.odf(sphere)
 print 'Visualizing CSA ODFs...'
 ren = fvtk.ren()
 fvtk.add(ren, fvtk.sphere_funcs(csaodfs, sphere, colormap='jet'))
-fvtk.show(ren)
+#fvtk.show(ren)
 # print('Saving illustration as csa_odfs.png')
 fvtk.record(ren, n_frames=1, out_path='csa_odfs.png',
             size=(600, 600), magnification=4)
@@ -69,7 +99,7 @@ print 'Visualizing CSA ODFs without negative values...'
 ren = fvtk.ren()
 fvtk.add(ren, fvtk.sphere_funcs(
     np.clip(csaodfs, 0, np.max(csaodfs, -1)[..., None]), sphere, colormap='jet'))
-fvtk.show(ren)
+#fvtk.show(ren)
 # print('Saving illustration as csa_odfs_positive.png')
 fvtk.record(ren, n_frames=1, out_path='csa_odfs_positive.png',
             size=(600, 600), magnification=4)
@@ -112,9 +142,26 @@ print 'Visualizing CSD ODFs ...'
 
 ren = fvtk.ren()
 fvtk.add(ren, fvtk.sphere_funcs(csd_odf, sphere, scale=1.3, norm=False, colormap='jet'))
-fvtk.show(ren)
+#fvtk.show(ren)
 # print('Saving illustration as csa_odfs_positive.png')
 fvtk.record(ren, n_frames=1, out_path='csd_odfs.png',
+            size=(600, 600), magnification=4)
+fvtk.clear(ren)
+
+
+sdt_model = ConstrainedSDTModel(gtab, ratio=(evals.min()/evals.max()))
+
+sdt_fit = sdt_model.fit(data_small)
+
+sdt_odf = sdt_fit.odf(sphere)
+
+print 'Visualizing SDT ODFs ...'
+
+ren = fvtk.ren()
+fvtk.add(ren, fvtk.sphere_funcs(sdt_odf, sphere, colormap='jet'))
+#fvtk.show(ren)
+# print('Saving illustration as csa_odfs_positive.png')
+fvtk.record(ren, n_frames=1, out_path='sdt_odfs.png',
             size=(600, 600), magnification=4)
 fvtk.clear(ren)
 
