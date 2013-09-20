ee28ed44beec3b1141937a446b0ff6030d77bced
Eleftherios Garyfallidis <garyfallidis@gmail.com>
2013-09-20 14:12:20 -0400

    Some code for a possible new picture

--- code/eudx_tracking.py
+++ code/eudx_tracking.py
@@ -1,16 +1,8 @@
 import nibabel as nib
 from nibabel import trackvis as tv
+from dipy.viz import fvtk
 
 dname = "/home/eleftherios/Data/Dave_St-Amour/results/"
-#fodf_sh = dname + "fodf.nii.gz"
-ftrk = dname + "whole_brain_wm_ants.trk"
-
-# img = nib.load(fodf_sh)
-# data = img.get_data()
-# affine = img.get_affine()
-
-from dipy.viz import fvtk
-ren = fvtk.ren()
 
 """
 T1
@@ -20,12 +12,13 @@ img = nib.load(fT1)
 data_T1 = img.get_data()
 affine_T1 = img.get_affine()
 
-I, J, K = data_T1.shape
-
-fvtk.add(ren, fvtk.slicer(data_T1, plane_i=[I / 2], plane_j=[J / 2], plane_k=[K / 2]))
-
-""" Load Streamlines
 """
+Load Streamlines
+"""
+
+# ftrk = dname + "whole_brain_wm_ants.trk"
+ftrk = dname + "whole_brain_wm_ants_cing.trk"
+ren = fvtk.ren()
 
 from dipy.reconst.shm import sh_to_sf
 from dipy.reconst.odf import peaks_from_model
@@ -38,26 +31,24 @@ print(len(streamlines))
 
 from dipy.tracking.vox2track import track_counts
 
-
-""" Create intersection roi
+"""
+Create intersection roi
 """
 
-roi_width = 10
+def create_roi(data_shape, pos, rw, value=1):
+    i, j, k = pos
+    roi = np.zeros(data_shape)
+    roi[i-rw:i+rw, j-rw:j+rw, k-rw:k+rw] = value
+    return roi
 
-roi = np.zeros(data_T1.shape)
-roi[90-roi_width:90+roi_width, 122-roi_width:122+roi_width, 66-roi_width:66+roi_width] = 1
 
-tcs,tes = track_counts(streamlines, roi.shape, (1,1,1), True)
-
-#find volume indices of mask's voxels
-roiinds=np.where(roi==1)
-
-#make it a nice 2d numpy array (Nx3)
-roiinds=np.array(roiinds).T
-
-def filter_roi_tracks(tracks, roiinds, tes):
+def filter_roi_tracks(tracks, roi, value=1):
     """ bring the tracks from the roi region and their indices
     """
+    tcs,tes = track_counts(streamlines, roi.shape, (1, 1, 1), True)
+    roiinds=np.where(roi==value)
+    roiinds=np.array(roiinds).T
+
     cnt=0
     sinds=[]
     for vox in roiinds:
@@ -68,7 +59,67 @@ def filter_roi_tracks(tracks, roiinds, tes):
             pass
     return [tracks[i] for i in list(set(sinds))], list(set(sinds))
 
-streamlines, sinds = filter_roi_tracks(streamlines, roiinds, tes)
+# splenium
+
+# pos = (90, 122, 66)
+# I, J, K = pos
+
+# roi = create_roi(data_T1.shape, pos=pos, rw=10)
+# streamlines, sinds = filter_roi_tracks(streamlines,roi)
+
+# pos2 = (80, 122, 76)
+# roi = create_roi(data_T1.shape, pos=pos2, rw=5)
+# streamlines, sinds = filter_roi_tracks(streamlines,roi)
+
+# pos3 = (110, 122, 76)
+# roi = create_roi(data_T1.shape, pos=pos3, rw=5)
+# streamlines, sinds = filter_roi_tracks(streamlines,roi)
+
+# CC far
+
+# pos = (62, 121, 99)
+# I, J, K = pos
+
+# roi = create_roi(data_T1.shape, pos=pos, rw=10)
+# streamlines, sinds = filter_roi_tracks(streamlines,roi)
+
+# pos2 = (120, 121, 99)
+
+# roi = create_roi(data_T1.shape, pos=pos2, rw=10)
+# streamlines, sinds = filter_roi_tracks(streamlines,roi)
+
+# cingulum
+
+# pos = (85, 121, 81)
+# I, J, K = pos
+
+# roi = create_roi(data_T1.shape, pos=pos, rw=10)
+# streamlines, sinds = filter_roi_tracks(streamlines, roi)
+
+# pos2 = (85, 131, 70)
+
+# roi = create_roi(data_T1.shape, pos=pos2, rw=10)
+# streamlines, sinds = filter_roi_tracks(streamlines, roi)
+
+# pos3 = (83, 82, 83)
+
+# roi = create_roi(data_T1.shape, pos=pos3, rw=10)
+# streamlines, sinds = filter_roi_tracks(streamlines, roi)
+
+# pos4 = (80, 69, 78)
+
+# roi = create_roi(data_T1.shape, pos=pos4, rw=10)
+# streamlines, sinds = filter_roi_tracks(streamlines, roi)
+
+# pos5 = (81, 62, 63)
+
+# roi = create_roi(data_T1.shape, pos=pos5, rw=10)
+# streamlines, sinds = filter_roi_tracks(streamlines, roi)
+
+
+# streams2 = [(s, None, None) for s in streamlines]
+
+# tv.write(dname + "whole_brain_wm_ants_2.trk", streams2, hdr)
 
 print(len(streamlines))
 
@@ -98,16 +149,14 @@ from dipy.viz.colormap import line_colors
 
 # final_streamlines = [streamlines[id] for id in streamline_ids]
 
-
-
 fvtk.add(ren, fvtk.streamtube(streamlines,
-         line_colors(streamlines), linewidth=2*0.15))
+         line_colors(streamlines), linewidth=2 * 0.15))
 
 
-
-
+fvtk.add(ren, fvtk.slicer(data_T1,
+                          plane_i=[data_T1.shape[0]/2],
+                          plane_j=None,
+                          plane_k=None,
+                          outline=False))
 
 fvtk.show(ren)
-
-
-
